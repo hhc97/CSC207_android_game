ebf4bda8e7258fad53b437d4436256d942ddfd78
Haocheng Hu <hc.hu@utoronto.ca>
2019-10-20 18:54:18 -0400

    added start game button

--- phase1/app/src/main/java/com/example/phase1/MainActivity.java
+++ phase1/app/src/main/java/com/example/phase1/MainActivity.java
@@ -8,7 +8,8 @@ import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String EXTRA_MESSAGE = "com.example.phase1.MESSAGE";
+  public static final String EXTRA_MESSAGE = "com.example.phase1.MESSAGE";
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -16,10 +17,12 @@ public class MainActivity extends AppCompatActivity {
   }
   /** Called when the user taps the Send button */
   public void sendMessage(View view) {
-      Intent intent = new Intent(this, DisplayMessageActivity.class);
-      EditText editText = (EditText) findViewById(R.id.editText);
-      String message = editText.getText().toString();
-      intent.putExtra(EXTRA_MESSAGE, message);
-      startActivity(intent);
+    Intent intent = new Intent(this, DisplayMessageActivity.class);
+    EditText editText = (EditText) findViewById(R.id.editText);
+    String message = editText.getText().toString();
+    intent.putExtra(EXTRA_MESSAGE, message);
+    startActivity(intent);
   }
+
+  public void startGame(View view) {}
 }
