56a2b0aed2a88a3f39051cb2ab0fb713e81e700b
Haocheng Hu <hc.hu@utoronto.ca>
2019-10-26 17:31:49 -0400

    removed DisplayMessage

--- phase1/app/src/main/java/com/example/phase1/MainActivity.java
+++ phase1/app/src/main/java/com/example/phase1/MainActivity.java
@@ -17,14 +17,6 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
     setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
   }
-  /** Called when the user taps the Send button */
-  public void sendMessage(View view) {
-    Intent intent = new Intent(this, DisplayMessageActivity.class);
-    EditText editText = findViewById(R.id.editText);
-    String message = editText.getText().toString();
-    intent.putExtra(EXTRA_MESSAGE, message);
-    startActivity(intent);
-  }
 
   public void startGame(View view) {
     Intent intent = new Intent(this, SaveMenu.class);
