218a9b5a96b6bba83a7cfe6be6df79d1b88a577b
Haocheng Hu <hc.hu@utoronto.ca>
2019-10-26 16:36:35 -0400

    add method for transmitting name

--- phase1/app/src/main/java/com/example/phase1/SetCharacterName.java
+++ phase1/app/src/main/java/com/example/phase1/SetCharacterName.java
@@ -2,13 +2,23 @@ package com.example.phase1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class SetCharacterName extends AppCompatActivity {
-
+  public static final String NAME = "com.example.phase1.NAME";
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_set_character_name);
+  }
+  public void sendName(View view) {
+    Intent intent = new Intent(this, SaveMenu.class);
+    EditText editText = findViewById(R.id.editText);
+    String message = editText.getText().toString();
+    intent.putExtra(NAME, message);
+    startActivity(intent);
   }
 }
