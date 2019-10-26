4d0cd9cc85622ac0e217ba82b4e4fdb231477491
Roland Zhao <roland.zhao@mail.utoronto.ca>
2019-10-26 18:48:54 -0400

    Added the ground that moves for Level 2.

--- phase1/app/src/main/java/com/example/phase1/Level2Activity.java
+++ phase1/app/src/main/java/com/example/phase1/Level2Activity.java
@@ -1,8 +1,11 @@
 package com.example.phase1;
 
+import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
 
 public class Level2Activity extends GameManager {
 
@@ -19,5 +22,22 @@ public class Level2Activity extends GameManager {
     this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
     setContentView(R.layout.activity_level2);
+
+    final ImageView backgroundOne = findViewById(R.id.imageView10);
+
+    final ValueAnimator animator = ValueAnimator.ofFloat(0.0f, 1.0f);
+    animator.setRepeatCount(ValueAnimator.INFINITE);
+    animator.setInterpolator(new LinearInterpolator());
+    animator.setDuration(10000L);
+    animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+      @Override
+      public void onAnimationUpdate(ValueAnimator animation) {
+        final float progress = (float) animation.getAnimatedValue();
+        final float width = backgroundOne.getWidth();
+        final float translationX = width * progress;
+        backgroundOne.setTranslationX(-translationX);
+      }
+    });
+    animator.start();
   }
 }
