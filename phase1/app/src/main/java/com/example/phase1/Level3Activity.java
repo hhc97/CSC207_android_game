2c64d0dbec8b926c06c10b1a8952f19a20843e65
Nayan Saxena <photosbynayan@gmail.com>
2019-10-29 04:43:52 -0400

    Created front-end for Level3 along with required code in Level3Activity

--- phase1/app/src/main/java/com/example/phase1/Level3Activity.java
+++ phase1/app/src/main/java/com/example/phase1/Level3Activity.java
@@ -2,6 +2,8 @@ package com.example.phase1;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.example.phase1.BackendStorage.GameManager;
 
@@ -12,6 +14,19 @@ public class Level3Activity extends GameManager {
     super.onCreate(savedInstanceState);
     Intent intent = getIntent();
     setCurrPlayer(intent.getIntExtra("com.example.phase1.SEND_PLAYER", 0));
-    setContentView(R.layout.activity_level3);
+
+    // Set our window to fullscreen without the bar at the top.
+    this.getWindow()
+            .setFlags(
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+    // Remove the title.
+    this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+    if (getDayOrNight() == 0) {
+      setContentView(R.layout.n_activity_level3);
+    } else if (getDayOrNight() == 1) {
+      setContentView(R.layout.activity_level3);
+    }
   }
 }
