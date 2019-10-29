4dfe1b0d617c2f7732682212e165f990e1fc96ce
Roland Zhao <roland.zhao@mail.utoronto.ca>
2019-10-28 23:52:12 -0400

    Added score text for Level 2. Added code that keeps track of movement in realtime for Level 2.

--- phase1/app/src/main/java/com/example/phase1/Level2Manager.java
+++ phase1/app/src/main/java/com/example/phase1/Level2Manager.java
@@ -1,24 +1,35 @@
 package com.example.phase1;
 
+import android.util.DisplayMetrics;
+import android.view.View;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Level2Manager {
+  private float dpHeight;
+  private float dpWidth;
   private int groundHeight = 0;
   List<GameObject> Obstacles = new ArrayList<>();
-  static Hero player;
-  private int playerStartX = 0;
+  private static Hero player;
+  private int playerStartX = 140;
   private int playerStartY = groundHeight;
 
-  public Level2Manager() {
+  public Level2Manager(View context) {
     player = new Hero(playerStartX, playerStartY);
     player.setStates(true);
-    GameObject rock1 = new GameObject(50, groundHeight);
-    GameObject rock2 = new GameObject(80, groundHeight);
-    GameObject rock3 = new GameObject(120, groundHeight);
+
+    GameObject rock1 = new GameObject(182, groundHeight);
+    GameObject rock2 = new GameObject(430, groundHeight);
+    GameObject rock3 = new GameObject(640, groundHeight);
+
     Obstacles.add(rock1);
     Obstacles.add(rock2);
     Obstacles.add(rock3);
+
+    DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+    dpHeight = displayMetrics.heightPixels / displayMetrics.density;
+    dpWidth = displayMetrics.widthPixels / displayMetrics.density;
   }
 
   public void update() {
