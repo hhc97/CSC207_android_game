17354c29981e97d08c4f9567ef7d01b09ac16173
Gavin Fang <gavin.fang@mail.utoronto.ca>
2019-11-25 20:45:12 -0500

    Merged checkWin and checkError. Changed return type of merged checkWin to int

--- phase2/app/src/main/java/com/example/phase1/Level3Game/Level3Manager.java
+++ phase2/app/src/main/java/com/example/phase1/Level3Game/Level3Manager.java
@@ -49,12 +49,32 @@ class Level3Manager {
     input.clear();
   }
 
+//  /**
+//   * Check if the user has made an error in the input of the sequence
+//   *
+//   * @return true if the user input does not match the sequence
+//   */
+//  boolean checkError() {
+//    Iterator<Integer> seq = sequence.iterator();
+//    Iterator<Integer> in = input.iterator();
+//
+//    while (seq.hasNext() && in.hasNext()) {
+//      int a = seq.next();
+//      int b = in.next();
+//      if (a != b) {
+//        attempts++;
+//        return true;
+//      }
+//    }
+//    return false;
+//  }
+
   /**
-   * Check if the user has made an error in the input of the sequence
+   * Check if the user has won or inputted the wrong sequence
    *
-   * @return true if the user input does not match the sequence
+   * @return 1 if user input does not match sequence, 2 if the user has won, else 0
    */
-  boolean checkError() {
+  int checkWin() {
     Iterator<Integer> seq = sequence.iterator();
     Iterator<Integer> in = input.iterator();
 
@@ -63,28 +83,12 @@ class Level3Manager {
       int b = in.next();
       if (a != b) {
         attempts++;
-        return true;
+        return 1;
       }
-    }
-    return false;
-  }
-
-  /**
-   * Check if the user has won
-   *
-   * @return true if user input does not match sequence
-   */
-  boolean checkWin() {
-    Iterator<Integer> seq = sequence.iterator();
-    Iterator<Integer> in = input.iterator();
-
-    while (seq.hasNext() && in.hasNext()) {
-      seq.next();
-      in.next();
       if (!seq.hasNext() && !in.hasNext()) {
-        return true;
+        return 2;
       }
     }
-    return false;
+    return 0;
   }
 }
