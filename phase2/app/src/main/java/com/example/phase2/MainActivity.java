a7fcf6a5ce5134dac9e7cab534091a5e41ae6790
Nayan Saxena <photosbynayan@gmail.com>
2019-12-01 09:52:06 -0500

    Revamped the start screen for the game, added better text and background components.

--- phase2/app/src/main/java/com/example/phase2/MainActivity.java
+++ phase2/app/src/main/java/com/example/phase2/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.phase2;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,7 +17,17 @@ public class MainActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
+
+    // Set our window to fullscreen without the bar at the top.
+    this.getWindow()
+            .setFlags(
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+    // Remove the title.
+    this.requestWindowFeature(Window.FEATURE_NO_TITLE);
     setContentView(R.layout.activity_main);
+
   }
 
   /**
