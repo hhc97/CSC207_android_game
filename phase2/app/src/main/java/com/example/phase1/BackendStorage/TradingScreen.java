2b2ffe754af25342f9d1a29cb566faeed4fa72be
Haocheng Hu <hc.hu@utoronto.ca>
2019-12-01 17:01:36 -0500

    added call to set currPlayer when trading screen is instantiated

--- phase2/app/src/main/java/com/example/phase1/BackendStorage/TradingScreen.java
+++ phase2/app/src/main/java/com/example/phase1/BackendStorage/TradingScreen.java
@@ -1,7 +1,5 @@
 package com.example.phase1.BackendStorage;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 import com.example.phase1.R;
@@ -10,11 +8,12 @@ import com.example.phase1.R;
  * An activity that allows players to trade their coins for items after they finish their first run
  * of the game.
  */
-public class TradingScreen extends AppCompatActivity {
+public class TradingScreen extends GameManager {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_trading_screen);
+    setCurrPlayer(getIntent().getIntExtra(sendPlayer, 0));
   }
 }
